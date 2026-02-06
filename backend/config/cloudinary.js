b4d50f115efcb57a0805e32368cf172abe2b80ff
Puspendra Singh <143992121+puspendrasingh639697@users.noreply.github.com>
2026-02-06 13:40:06 +0500

    Backend readyth admn lgin

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,11 @@
-const cloudinary = require('cloudinary').v2; 
+const cloudinary = require('cloudinary').v2;
 
+// dotenv.config() live server par kabhi-kabhi issue karta hai
+// Isliye seedha process.env use karo
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_KEY,
   api_secret: process.env.CLOUD_SECRET
 });
 
-module.exports = cloudinary; 
+module.exports = cloudinary;
